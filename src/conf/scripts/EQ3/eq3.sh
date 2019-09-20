d49f4a9e8e8d276517abc71981760fd337571ef1
smarthome-w <smarthome.mail>
2019-09-20 21:15:13 +0200

    fixes-12

--- src/conf/scripts/EQ3/eq3.sh
+++ src/conf/scripts/EQ3/eq3.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#Bedroom
+echo Bedroom
 /usr/bin/mosquitto_pub -h 192.168.0.4 -t '/bedroomradin/trv' -m '00:1A:22:10:0C:C7 unlock'
 sleep 20
-#AnnRoom
+echo AnnRoom
 /usr/bin/mosquitto_pub -h 192.168.0.4 -t '/bedroomradin/trv' -m '00:1A:22:10:0C:7B unlock'
 sleep 20
-#AliceRoom
-/usr/bin/mosquitto_pub -h 192.168.0.4 -t '/bedroomradin/trv' -m '00:1A:22:10:09:0C unlock'
-sleep 20
-#FamilyRoom
+#echo AliceRoom
+#/usr/bin/mosquitto_pub -h 192.168.0.4 -t '/bedroomradin/trv' -m '00:1A:22:10:09:0C unlock'
+#sleep 20
+echo FamilyRoom
 /usr/bin/mosquitto_pub -h 192.168.0.4 -t '/bedroomradin/trv' -m '00:1A:22:0C:85:FA unlock'
-sleep 20
-#Kitchen
-/usr/bin/mosquitto_pub -h 192.168.0.4 -t '/bedroomradin/trv' -m '00:1A:22:0E:06:A0 unlock'
+#sleep 20
+#echo Kitchen
+#/usr/bin/mosquitto_pub -h 192.168.0.4 -t '/bedroomradin/trv' -m '00:1A:22:0E:06:A0 unlock'
+echo done
