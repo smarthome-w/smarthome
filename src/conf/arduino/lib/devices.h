75a72f738aadac81e96bc5de8dfc91b80be7e5b8
smarthome-w <smarthome.mail>
2020-04-22 13:00:57 +0200

    fixes-30

--- src/conf/arduino/lib/devices.h
+++ src/conf/arduino/lib/devices.h
@@ -36,17 +36,20 @@
 //#define FamilyRoomLight
 //#define CorridorStorage
 
-//#define Kitchen // migrated to tasmota
+//#define TechnicalRoom1
+//#define TechnicalRoom2
+//#define FamilyRoomRollershutter
+//#define BedroomRollershutter
 //#define KitchenLED
+
+//#define Kitchen // migrated to tasmota
 //#define BedroomLights // migrated to tasmota
 //#define BedroomWardobe
 //#define CorridorLights
 //#define AnnRoom
 //#define AliceRoom
 //#define Bedroom // change CIR to D5 instead D6 (hardware issue) // migrated to
-// tasmota #define FamilyRoom #define TechnicalRoom1 #define TechnicalRoom2
-#define FamilyRoomRollershutter
-//#define BedroomRollershutter
+// tasmota #define FamilyRoom
 //#define TestRoom
 //#define TestRelay
 //#define TestStepper
--- src/conf/arduino/lib/heartbeat.h
+++ src/conf/arduino/lib/heartbeat.h
@@ -2,6 +2,7 @@
 #define heartbeat_h
 #define HEARTBEAT_READ_INTERVAL_MILLIS 60000
 int HeartbeatLastReadInMillis = 0;
+int millisCycles = 0;
 
 void sendHeartbeatMQTTMessage(float milliseconds) {
   String messageTopic = GLOBAL_MQTT_MESSAGE_PREFIX + "/" +
@@ -25,11 +26,30 @@ int WifiGetRssiAsQuality(int rssi) {
   return quality;
 }
 
+void print_uint64_t(uint64_t num) {
+
+  char rev[128];
+  char *p = rev + 1;
+
+  while (num > 0) {
+    *p++ = '0' + (num % 10);
+    num /= 10;
+  }
+  p--;
+  /*Print the number which is now in reverse*/
+  while (p > rev) {
+    Serial.print(*p--);
+  }
+}
+
 void sendHeartbeatInfoMQTTMessage() {
   String messageTopic = "tele/" + GLOBAL_MQTT_MULTISENSOR_NAME + "/STATE";
   int signalStrength = WifiGetRssiAsQuality(WiFi.RSSI());
 
-  String messageValue = "{\"Time\":\"" + String(millis()) +
+  uint32_t a_millis = millis();
+  uint64_t uptime = millisCycles * 0x100000000 + a_millis;
+  float uptime_days = 1.0 * uptime / 86400000.0;
+  String messageValue = "{\"Uptime\":\"" + String(uptime_days, 4) +
                         "\",\"FreeMem\":" + String(ESP.getFreeHeap()) +
                         ",\"Wifi\":{\"RSSI\":" + String(signalStrength) +
                         ",\"MAC\":\"" + String(WiFi.macAddress()) + "\"}}";
@@ -39,6 +59,9 @@ void sendHeartbeatInfoMQTTMessage() {
 void processHeartbeat() {
   if (abs(millis() - HeartbeatLastReadInMillis) >
       HEARTBEAT_READ_INTERVAL_MILLIS) {
+    if (millis() < HeartbeatLastReadInMillis) {
+      millisCycles++;
+    }
     sendHeartbeatMQTTMessage(millis());
     sendHeartbeatInfoMQTTMessage();
     HeartbeatLastReadInMillis = millis();
