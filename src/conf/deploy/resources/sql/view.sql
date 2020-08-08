e92cef0825e03d54fa2f8e78c3aba91e92af532f
smarthome-w <smarthome.mail>
2020-08-08 13:06:09 +0200

    fixes-32

--- src/conf/deploy/resources/sql/view.sql
+++ src/conf/deploy/resources/sql/view.sql
@@ -8,3 +8,10 @@ create or replace view Item180
 as
 select timestamp(theday) as time, consumption as value
 from power_consumption;
+
+
+//select CONCAT(YEAR(theday), '-', MONTH(theday)) as power_month, sum(consumption)
+//from power_consumption
+//group by power_month
+//order by power_month asc
+
