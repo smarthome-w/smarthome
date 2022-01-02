9fc0e3198ad4614068b98e8d7f47d93bd02cd806
smarthome-w <smarthome.mail>
2022-01-02 20:35:42 +0100

    cvs

---
+++ src/conf/scripts/GOOGLETTS/detect_error.sh
@@ -0,0 +1,13 @@
+#!/bin/bash +e
+
+PHRASE=$@
+cat /openhab/userdata/logs/openhab.log | grep -i "Error saying" | tail -n 1 | grep "$PHRASE"
+RET_VAL=$?
+if [[ "$RET_VAL" == "0" ]]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') GoogleError - \"$PHRASE\"" | tee -a /openhab/userdata/logs/openhab.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') error saying - reset error indicator" | tee -a /openhab/userdata/logs/openhab.log
+    exit 1
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') GoogleOK - \"$PHRASE\"" | tee -a /openhab/userdata/logs/openhab.log
+    exit 0
+fi
