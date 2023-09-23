35318d595efa809f1a12c3a389297716b4697212
aldona <valldeck@poczta.onet.pl>
2023-09-23 19:43:30 +0200

    cvs

--- src/conf/transform/tasmotaAnalogReverse.js
+++
@@ -1,7 +0,0 @@
-(function (data) {
-  var dataString = JSON.parse(data);
-  var value = "UNDEF";
-  value = dataString.ANALOG.A0;
-  value = 1024 - value;
-  return value.toString(10);
-})(input);
--- src/conf/transform/tasmotaBME280Pressure.js
+++
@@ -1,7 +0,0 @@
-(function (data) {
-  var dataString = JSON.parse(data);
-  var value = "UNDEF";
-  value = dataString.BME280.Pressure;
-  value = value + 17.59;
-  return value.toString(10);
-})(input);
